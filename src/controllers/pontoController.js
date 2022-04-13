18c27335d16de292d478163f02a35cdc35318c3b
unknown <matheuscolombo@univem.edu.br>
2022-04-13 08:29:46 -0300

    adicionando endpoints

--- src/controllers/pontoController.js
+++ src/controllers/pontoController.js
@@ -11,13 +11,38 @@ router.post('/register', async(req,res)=> {
         return res.status(400).send({error:'ponto turistico ja existe'});
 
         const ponto = await Ponto.create(req.body);
-        console.log(req.body);
 
         return res.send({ponto})
     }catch(err){
         return res.status(400).send({error : 'Registration failed'});
     }
 });
+
+router.delete('/deleteByNome', async (req,res) => {
+    const {nome} = req.body;
+    try{
+        if (await !Ponto.findOne({nome}))
+        return res.status(400).send({error: 'ponto turistico nao existe'})
+
+        const deleted = await Ponto.deleteOne({ nome : nome});
+        return res.send({deleted});
+    }catch(err){
+        return res.status(400).send({error : 'Deletion failed'});
+    }
+}); 
+
+/* GET ALL PONTOS TURISTICOS  BY ESTADO */
+router.post('/estado' , async (req,res) => {
+    try{
+        const { estado } = req.body;
+
+    const est = await Ponto.find({ estado: estado }).exec();
+    return res.send({est});
+    }catch(err){
+        return res.status(400).send({ error : 'error fectching pontos turisticos'})
+    }
+});
+
 
 router.get('/all', async (req,res) =>{
     const all = await Ponto.find({});
